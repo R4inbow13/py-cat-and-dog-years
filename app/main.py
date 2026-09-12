e7b85ad0637d6fdd59495bf6c605a2d7e6accad6
Stas Makarov <makarov.st@icloud.com>
2026-09-12 11:42:49 +0300

    solution1

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     else:
         result[0] += (cat_age - 24) // 4 + 2
 
-    if cat_age < 15:
+    if dog_age < 15:
         result[0] += 0
     elif 15 <= dog_age < 24:
         result[1] += 1
--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,44 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_should_return_zero_for_ages_below_15() -> None:
-    assert get_human_age(5, 8) == [0, 0]
-
-
-def test_should_return_1_for_ages_between_15_and_24() -> None:
-    assert get_human_age(16, 23) == [1, 1]
-
-
-def test_should_return_2_if_ages_between_24_and_28() -> None:
-    assert get_human_age(25, 27) == [2, 2]
-
-
-def test_should_return_3_and_2_if_ages_equal_28() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_return_21_and_17_if_ages_equal_100() -> None:
-    assert get_human_age(100, 100) == [21, 17]
-
-
-def test_if_ages_different_ages_of_cat_and_dog() -> None:
-    assert get_human_age(75, 46) == [14, 6]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_ages",
+    [
+        pytest.param(
+            5, 8,
+            [0, 0],
+            id="test should return zero for ages below 15"
+        ),
+        pytest.param(
+            16, 23,
+            [1, 1],
+            id="test should return 1 for ages between 15 and 24"
+        ),
+        pytest.param(
+            25, 27,
+            [2, 2],
+            id="test should return 2 if ages between 24 and 28"
+        ),
+        pytest.param(
+            28, 28,
+            [3, 2],
+            id="test should return 3 and 2 if ages equal 28"
+        ),
+        pytest.param(
+            100, 100,
+            [21, 17],
+            id="test should return 21 and 17 if ages equal 100"
+        ),
+        pytest.param(
+            75, 46,
+            [14, 6],
+            id="test if ages different ages of cat and dog"
+        )
+    ]
+)
+def test_should_convert_ages_to_human_years(cat_age: int,
+                               dog_age: int,
+                               expected_ages: list[int]
+                               ) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_ages
