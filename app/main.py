f04a7100d233d4c870bfc6b527df863edd62fb0e
Stas Makarov <makarov.st@icloud.com>
2026-09-12 11:48:59 +0300

    solution3

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         result[0] += (cat_age - 24) // 4 + 2
 
     if dog_age < 15:
-        result[0] += 0
+        result[1] += 0
     elif 15 <= dog_age < 24:
         result[1] += 1
     else:
